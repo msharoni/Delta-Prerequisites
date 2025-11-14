b71a644f380ab83783e2ca3d4070fefb0e0d40e9
msharoni <92259206+msharoni@users.noreply.github.com>
2025-11-14 18:16:18 +0000

    assembly 2

---
+++ assembly excercises/assembly 2.asm
@@ -0,0 +1,10 @@
+mov eax,0   ;eax = 0
+add eax,1   ;eax = 1
+add eax,2   ;eax = 3
+add eax,3   ;eax = 6
+
+;after this part od the code eax = 6
+
+xor eax,eax ;eax = 0 because any number xor with itself is 0
+mov ebx,eax ;ebx = eax = 0
+dec ebx     ;ebx -= 1 -> ebx = 0XFFFFFFFF = -1
