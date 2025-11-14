7d8a636b8c0aa6fd74349b0dfe76e8e7fa1ffd21
msharoni <92259206+msharoni@users.noreply.github.com>
2025-11-14 18:19:27 +0000

    spelling mistake

--- assembly excercises/assembly 2.asm
+++ assembly excercises/assembly 2.asm
@@ -3,7 +3,7 @@ add eax,1   ;eax = 1
 add eax,2   ;eax = 3
 add eax,3   ;eax = 6
 
-;after this part od the code eax = 6
+;after this part of the code eax = 6
 
 xor eax,eax ;eax = 0 because any number xor with itself is 0
 mov ebx,eax ;ebx = eax = 0
